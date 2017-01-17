a9bdc795b176d07090644abc359e445c50909065
Codi257 <lsviper257@yahoo.com>
2017-01-16 22:41:40 -0600

    Additional UDOO Serial Support
    Can check for serial connections from auxiliary iMX and SAM processor.
    Prints verification or error to console and to JSON file.

--- python_serial_data_processing.py
+++ python_serial_data_processing.py
@@ -18,38 +18,89 @@ except ImportError:
     import json
     print ("simplejson import error")
 
+'''***************************************************************'''
+''' Start all Serial connections '''
+'''***************************************************************'''
+''' Initialize Serial Connection to Primary SAM3X8E Procesor '''
+try:
+    ser = serial.Serial(
+        port='/dev/ttyACM0',    # Port for Raspberry Pi
+        # port='dev/tty6',      # Port for UDOO
+        baudrate=115200,
+        parity='N',
+        #stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=None
+        )
 
-''' Initialize Serial Connection to Connected SAM3X8E Procesor '''
-ser = serial.Serial(
-    port='/dev/ttyACM0',    # Port for Raspberry Pi
-    # port='dev/tty6',      # Port for UDOO
-    baudrate=115200,
-    parity='N',
-    #stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=None
-    )
+    if  ser.isOpen():
+        print 'Primary SAM3X8E Serial Connected'
+    else:
+        print "WARNING: Primary SAM3X8E Processor Serial Disconnected"
+except:
+    print "WARNING: Primary SAM3X8E Processor Serial Failure"
 
-if  ser.isOpen():
-    print 'Serial connected'
 
+''' Initialize Serial Connection to Auxiliary iMX6 Procesor '''
+try:
+    ser1 = serial.Serial(
+        # port='dev/tty6',      # Port for UDOO
+        baudrate=115200,
+        parity='N',
+        #stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=None
+        )
+
+    if  ser1.isOpen():
+        print 'Auxiliary iMX6 Serial Connected'
+    else:
+        print "WARNING: Auxiliary iMX6 Processor Serial Disconnected"
+except:
+    print "WARNING: Auxiliary iMX6 Processor Serial Failure"
+
+
+''' Initialize Serial Connection to Auxiliary SAM3X8E Procesor '''
+try:
+    ser2 = serial.Serial(
+        # port='dev/tty6',      # Port for UDOO
+        baudrate=115200,
+        parity='N',
+        #stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=None
+        )
+
+    if  ser2.isOpen():
+        print 'Auxiliary SAM3X8E Serial Connected'
+    else:
+        print "WARNING: Auxiliary SAM3X8E Processor Serial Disconnected"
+except:
+    print "WARNING: Auxiliary SAM3X8E Processor Serial Failure"
+
+    
+'''***************************************************************'''
 ''' Declare all variables '''
-accelX = None
-accelY = None
-accelZ = None
 
-accelX_Hex = None
-accelY_Hex = None
-accelZ_Hex = None
+'''
+accelX = ''
+accelY = ''
+accelZ = ''
 
-accelX_Int = None
-accelY_Int = None
-accelZ_Int = None
+accelX_Hex = ''
+accelY_Hex = ''
+accelZ_Hex = ''
 
+accelX_Int = 1
+accelY_Int = 1
+accelZ_Int = 1
+'''
+
+'''***************************************************************'''
 ''' Declare all functions '''
 
 # Print Acceleration Values
-def printAccel():
+def printAccel(accelX, accelY, accelZ):
     print '\nX = '
     print "Byte Array = %s" % ([accelX])
     accelX_Hex = accelX.encode("hex")
@@ -73,17 +124,60 @@ def printAccel():
     accelZ_Int = int(accelZ_Hex, 16)
     print "Integer Value = %s" % accelZ_Int
 
+''' Works '''
+# Conversion function from serial string to int
+def toInt( valStr ):
+    valHex = valStr.encode("hex")
+    valInt = int(valHex, 16)
+    return valInt
+
+''' NOT COMPLETE '''
+# Conversion of Two's Complement
+'''
+def twoConvert ( int )
+    value = functionOf(int)
+    return value
+
+'''
+
+''' NOT COMPLETE - Partial '''
 # JSON Testing
 # Declare dump all json function
-def jsonDump():
+def jsonDump(accelX, accelY, accelZ):
+    accelX_Int = toInt(accelX)
+    accelY_Int = toInt(accelX)
+    accelZ_Int = toInt(accelX)
+    #
     time = str(datetime.datetime.now().time())
     data = {
         'timestamp' : time,
         'accelX' : accelX_Int,
         'accelY' : accelY_Int,
         'accelZ' : accelZ_Int,
-        'shares' : 108976,
-        'Price' : 234
+        'calSpeed' : 0,
+        'wheelSpeed' : 0,
+        'position' : 0,
+        'temp' : 0,
+        'pressure' : 0,
+        'batVoltage' : 0,
+        'batCurrent' : 0,
+        'angularRateX' : 0,
+        'angularRateY' : 0,
+        'angularRateZ' : 0,
+        'attitudeX' : "hopeful",
+        'attitudeY' : "enlightened",
+        'attitudeZ' : "joyful",
+        'dBrakePosition' : 0,
+        'dBrakeForce' : 0,
+        'magLevPosition' : 0,
+        'magBrakePosition' : 0,
+        'yawPosition' : 0,
+        'operationMode' : 0,
+        'systemErrors' : "All is according to plan (Not an actual value)",
+        'systemAlerts' : "Ignorance is Bliss (Not an actual value)",
+        'serialSAM1' : ser.isOpen(),
+        'serialSAM2' : ser2.isOpen(),
+        'serialIMX2' : ser1.isOpen()
         }
     with open('sam1data.json', mode='w') as f:
         json.dump(data, f)
@@ -117,9 +211,8 @@ Current (Amp)
 Disk Brake Position
 Disk Brake Force
 Mag Lev Position
-Mag Brak Position
+Mag Brake Position
 Yaw position
-
 
 
 Time
@@ -210,8 +303,9 @@ while True:
         if(end == '\x00\x03'):
             integrity = True
             print 'Sensor Data Verified'
-            printAccel()
-            jsonDump()
+            print accelX
+            printAccel(accelX, accelY, accelZ)
+            jsonDump(accelX, accelY, accelZ)
         ser.read(1)         # The extra byte before 16BIT "head"
         # print integrity
         print '\n'
