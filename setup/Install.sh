048ab614d7430d89abde9f5e9b2fb7ea249bf739
Codi257 <lsviper257@yahoo.com>
2017-01-19 16:17:43 -0600

    Updated Install Instructions

--- setup/Install.sh
+++ setup/Install.sh
@@ -7,14 +7,18 @@ apt-get upgrade
 # Install apache
 apt-get install -y apache2
 
+# Install PHP
+apt-get install -y php5 php5-cli
+
 # Set web permissions for www-data
 groupadd www-data
 usermod -g www-data www-data
 chown -R www-data:www-data /var/www
 
-# Mount web directory for udooer to edit
+# Mount web directory for udooer to edit (installs bindfs)
 apt-get -y install bindfs
 
 mkdir -p /home/udooer/hyperloop
+mkdir -p /home/udooer/hyperloop/web
 chown -Rf udooer:udooer /home/udooer/hyperloop
 chmod -Rf 770 /home/udooer/hyperloop
