09e59ab33e62decab83d40cee84b1a3831c0e921
Codi257 <lsviper257@yahoo.com>
2017-01-19 00:13:46 -0600

    Main Loop, Logging, Threading
    Major Revision: The entire main loop has been converted to function
    calls. Can read commands from text file. Added support for logging and
    some implementation. Started support for threading.

--- python_serial_data_processing.py
+++ python_serial_data_processing.py
@@ -1,25 +1,64 @@
 #! /usr/bin/python
 '''
     Created for Team Codex Hyperloop pod
-    Primarily edited by Codi West
-    1-10-17
+    Original code by Codi West
+    Created 1-10-17
 '''
 
 import serial
-import time
+#import time
 import datetime
-import struct
-import array
+#import struct
+#import array
 #import io
+import threading    # NOT Optimized YET
+import logging      # To be utilized
+import json
 
+'''
 try:
     import simplejson
 except ImportError:
     import json
-    print ("simplejson import error")
+    # print ("simplejson import error")
+'''
 
 '''***************************************************************'''
-''' Start all Serial connections '''
+''' Enable Logging '''
+
+logging.basicConfig(filename='logdata.log',format='%(asctime)s: %(levelname)s: %(message)s',level=logging.DEBUG)
+
+logging.info('Starting Python Program')
+
+
+'''***************************************************************'''
+''' Declare all variables - Decided to use globals '''
+
+# Serial Connections
+serUp = False
+ser1Up = False
+ser2Up = False
+
+
+accelX = ''
+accelY = ''
+accelZ = ''
+
+'''
+accelX_Hex = ''
+accelY_Hex = ''
+accelZ_Hex = ''
+
+accelX_Int = 1
+accelY_Int = 1
+accelZ_Int = 1
+'''
+
+'''***************************************************************'''
+
+
+'''***************************************************************'''
+''' Start all Serial connections (on start) '''
 '''***************************************************************'''
 ''' Initialize Serial Connection to Primary SAM3X8E Procesor '''
 try:
@@ -35,10 +74,14 @@ try:
 
     if  ser.isOpen():
         print 'Primary SAM3X8E Serial Connected'
+        #global serUp
+        serUp = True
     else:
         print "WARNING: Primary SAM3X8E Processor Serial Disconnected"
+        #global serUp
+        serUp = False
 except:
-    print "WARNING: Primary SAM3X8E Processor Serial Failure"
+    print "WARNING: Primary SAM3X8E Processor Serial FATAL Failure"
 
 
 ''' Initialize Serial Connection to Auxiliary iMX6 Procesor '''
@@ -54,10 +97,14 @@ try:
 
     if  ser1.isOpen():
         print 'Auxiliary iMX6 Serial Connected'
+        #global ser1Up
+        ser1Up = True
     else:
         print "WARNING: Auxiliary iMX6 Processor Serial Disconnected"
+        #global ser1Up
+        ser1Up = False
 except:
-    print "WARNING: Auxiliary iMX6 Processor Serial Failure"
+    print "WARNING: Auxiliary iMX6 Processor Serial FATAL Failure"
 
 
 ''' Initialize Serial Connection to Auxiliary SAM3X8E Procesor '''
@@ -73,39 +120,72 @@ try:
 
     if  ser2.isOpen():
         print 'Auxiliary SAM3X8E Serial Connected'
+        #global ser2Up
+        ser2Up = True
     else:
         print "WARNING: Auxiliary SAM3X8E Processor Serial Disconnected"
+        #global ser2Up
+        ser2Up = False
 except:
-    print "WARNING: Auxiliary SAM3X8E Processor Serial Failure"
+    print "WARNING: Auxiliary SAM3X8E Processor Serial FATAL Failure"
 
     
-'''***************************************************************'''
-''' Declare all variables '''
 
-'''
-accelX = ''
-accelY = ''
-accelZ = ''
-
-accelX_Hex = ''
-accelY_Hex = ''
-accelZ_Hex = ''
-
-accelX_Int = 1
-accelY_Int = 1
-accelZ_Int = 1
-'''
-
-'''***************************************************************'''
 ''' Declare all functions '''
 
-# Print Acceleration Values
-def printAccel(accelX, accelY, accelZ):
+#Check Serial Status
+def checkSerial( ):
+    # Serial
+    global serUp
+    if  ser.isOpen():
+        # print 'Primary SAM3X8E Serial Connected'
+        # global serUp
+        serUp = True
+    else:
+        # print "WARNING: Primary SAM3X8E Processor Serial Disconnected"
+        #global serUp
+        serUp = False
+    # Serial 1
+    global ser1Up
+    if  ser1.isOpen():
+        # print 'Auxiliary iMX6 Serial Connected'
+        # global ser1Up
+        ser1Up = True
+    else:
+        # print "WARNING: Auxiliary iMX6 Processor Serial Disconnected"
+        # global ser1Up
+        ser1Up = False
+    # Serial 2
+    global ser2Up
+    if  ser2.isOpen():
+        # print 'Auxiliary SAM3X8E Serial Connected'
+        # global ser2Up
+        ser2Up = True
+    else:
+        # print "WARNING: Auxiliary SAM3X8E Processor Serial Disconnected"
+        # global ser2Up
+        ser2Up = False
+
+
+''' Works '''
+# Conversion function from serial string to int (Complete)
+def toInt( valStr ):
+    valHex = valStr.encode("hex")
+    valInt = int(valHex, 16)
+    return valInt
+
+
+# Print Acceleration Values (Needs more data from Brandon)
+def printAccel():
+    global accelX
+    global accelY
+    global accelZ
     print '\nX = '
     print "Byte Array = %s" % ([accelX])
     accelX_Hex = accelX.encode("hex")
     print "Hex Value = %s" % accelX_Hex
     accelX_Int = int(accelX_Hex, 16)
+    #accelX_Int = toInt(accelX)
     print "Integer Value = %s" % accelX_Int
     #
     print '\nY = '
@@ -124,15 +204,9 @@ def printAccel(accelX, accelY, accelZ):
     accelZ_Int = int(accelZ_Hex, 16)
     print "Integer Value = %s" % accelZ_Int
 
-''' Works '''
-# Conversion function from serial string to int
-def toInt( valStr ):
-    valHex = valStr.encode("hex")
-    valInt = int(valHex, 16)
-    return valInt
 
 ''' NOT COMPLETE '''
-# Conversion of Two's Complement
+# Conversion of Two's Complement (Need info from Brandon)
 '''
 def twoConvert ( int )
     value = functionOf(int)
@@ -140,10 +214,59 @@ def twoConvert ( int )
 
 '''
 
+# Read from a text file from webserver
+def getCommand( ):
+    try:
+        file = open('web/command.txt', 'r')
+        # print file.read()
+        command = file.read()
+        return command
+    except:
+        # print "File not found"
+        logging.info('Command File NOT Found')
+        
+
+# Receive Current SAM Data through Serial (ser)
+def getSer( ):
+    global accelX
+    global accelY
+    global accelZ
+    
+    head = ""
+    head = ser.read(1)
+    # print ([head])    # Only needed for debugging
+    if head == '\x01':
+        integrity = False
+        # print '\n'
+        #print ('Start of sensor data')
+        logging.info('Start of sensor data')
+        #   Reads Accelerometer Sensor Data
+        accelX = ser.read(2)
+        accelY = ser.read(2)
+        accelZ = ser.read(2)
+        #   Tests End of Text (ETX)
+        end = ser.read(2)
+        #print ("Received data")
+        logging.info('Primary Serial Data Received')
+        # print ([end])
+        if(end == '\x00\x03'):
+            integrity = True
+            # print 'Sensor Data Verified'
+            logging.info('Sensor Data Verified')
+            printAccel()
+            jsonDump()
+        else:
+            # print "ALERT!!! Receive error from Primary SAM"
+            logging.info('ALERT!!! Receive error from Primary SAM')
+        # ser.read(1)         # The extra byte before 16BIT "head"
+        # print integrity
+        #print '\n'
+
+
 ''' NOT COMPLETE - Partial '''
 # JSON Testing
 # Declare dump all json function
-def jsonDump(accelX, accelY, accelZ):
+def jsonDump( ):
     accelX_Int = toInt(accelX)
     accelY_Int = toInt(accelY)
     accelZ_Int = toInt(accelZ)
@@ -181,9 +304,10 @@ def jsonDump(accelX, accelY, accelZ):
         }
     with open('web/sam1data.json', mode='w') as f:
         json.dump(data, f)
-    with open('web/archive/sam1data%s.json' % time, mode='w') as f:
+    with open('web/datalog/sam1data%s.json' % time, mode='w') as f:
         json.dump(data, f)
-    print datetime.datetime.now().time()
+    # print datetime.datetime.now().time()
+    logging.info('JSON Successfully Dumped')
 
 
 # Read all serial data function
@@ -194,19 +318,12 @@ Acceleration (Gyroscope)
     Y
     Z
 Calculated Speed
-
 Wheel Speed (Optosensor)
-
 Position (Calculated)
-
 Temperature
-
 Pressure
-
 Voltage
-
 Current (Amp)
-
 
 Disk Brake Position
 Disk Brake Force
@@ -215,7 +332,6 @@ Mag Brake Position
 Yaw position
 
 Time
-
 '''
 
 ''' Dump JSON Data Function
@@ -224,19 +340,12 @@ Acceleration (Gyroscope)
     Y
     Z
 Calculated Speed
-
 Wheel Speed (Optosensor)
-
 Position (Calculated)
-
 Temperature
-
 Pressure
-
 Voltage
-
 Current (Amp)
-
 
 Disk Brake Position
 Disk Brake Force
@@ -247,33 +356,20 @@ Yaw position
 Time
 '''
 
+
 ''' Potentially Dump MySQL Data
 '''
 
-#
-''' Receive Start Command
-
+'''
+    Receive Start Command
+    Receive Stop Command
+    Receive Self Test Command
+    Receive System Test Command
 '''
 
-#
-''' Receive Stop Command
-
-'''
-
-#
-''' Receive Self Test Command
-
-'''
-
-#
-''' Receive System Test Command
-
-'''
 
 ''' Check status of Other CPU and 2 MCUs
-
 '''
-
 ''' Send Command to MCUs and failover CPU
 switch:
     Wait is default
@@ -288,7 +384,35 @@ switch:
 '''***************************************************************'''
 # Must use with matching main.cpp in SAM3X8E-Libraries
 while True:
+    command = getCommand()
+    # Very dissapointed upon finding out Python does not have switch statements'
+    if(command == 'stop'):
+        # logging.warning('Received STOP command; Emergency Braking')
+        checkSerial()
+        # print 'stop'
+    elif(command == 'start'):
+        # logging.info('Received START command')
+        checkSerial()
+        # Send Start Command
+        getSer()
+        # printAccel()  # Contained in getSer()
+        # jsonDump()    # Contained in getSer()
+        # print 'start'
+    elif(command == 'systemtest'):
+        # logging.info('Received SYSTEMTEST command')
+        checkSerial()
+        # print 'testing serial connections'
+    elif(command == 'systemtest'):
+        # logging.info('Received SELFTEST command')
+        checkSerial()
+        # print 'testing connections'
+    else:
+        logging.info('Awaiting COMMAND: Running tests in the meantime')
+        checkSerial()
+        # print 'Not receiving any commands from HQ'
 
+
+        
     # Attempt multiple threads for separate serial connections - INCOMPLETE
     # Check Status of Serial Connections
         # Serial 0 (Primary SAM3X8E)
@@ -306,10 +430,14 @@ while True:
             # except print WARNING Secondary iMX not connected
         # try Serial 2 (Secondary SAM3X8E)
             #
-    ''' Reading SAM3X8E (Primary) serial - Create SEPARATE function'''
+    '''Reading SAM3X8E (Primary) serial - Create SEPARATE function'''
+
+''' Legacy
     # print "Locating start bit"
+    checkSerial()
     head = ""
-    head = ser.read(1)
+    # head = ser.read(1)
+    getSer()
     # print ([head])    # Only needed for debugging
     if head == '\x01':
         integrity = False
@@ -328,12 +456,13 @@ while True:
             print 'Sensor Data Verified'
             print accelX
             # printAccel(accelX, accelY, accelZ)
-            jsonDump(accelX, accelY, accelZ)
+            jsonDump()
         else:
             print "ALERT!!! Receive error"
         # ser.read(1)         # The extra byte before 16BIT "head"
         # print integrity
         print '\n'
+'''
 
 '''***************************************************************'''
 ''' Secondary UDOO LOOP - PSEUDOCODE '''
